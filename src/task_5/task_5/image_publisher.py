495123470b799e35cf700e5ced5a9bf85beb06ff
AlbertWeirdo <fualbert20030507@gmail.com>
2025-04-02 00:40:43 -0400

    complete the hsv mask

--- src/task_5/task_5/image_publisher.py
+++ src/task_5/task_5/image_publisher.py
@@ -2,20 +2,21 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
-import cv2 as cv
+import cv2
 
 class ImagePublisher(Node):
     def __init__(self, address_to_video):
         super().__init__('image_publisher')
         
         self.bridge = CvBridge()
-        self.publisher = self.create_publisher(Image ,'video_data', 10)
+        self.publisher = self.create_publisher(Image ,'/video_data', 10)
         self.address_to_video = address_to_video
-        self.processImage()
+        # self.create_timer(1, self.processImage())
+
         
     def processImage(self):
         # read video
-        cap = cv.VideoCapture(self.address_to_video)
+        cap = cv2.VideoCapture(self.address_to_video)
         
         while cap.isOpened():
             success, frame = cap.read()
@@ -25,24 +26,38 @@ class ImagePublisher(Node):
                 print('Can\'t receive frame. Exiting')
                 break
             
-            cv.imshow('frame', frame)
+            cv2.imshow('frame', frame)
 
             # convert the frame from opencv image to ros2 image
             ros2_image = self.bridge.cv2_to_imgmsg(frame)
             self.publisher.publish(ros2_image)
 
             # if the user press esc in the 25 milliseconds, the loop will stop
-            if cv.waitKey(25) == 27:
+            if cv2.waitKey(25) == 27:
                 break
         
         cap.release()
-        cv.destroyAllWindows()
+        cv2.destroyAllWindows()
+        return 
+
+
         
 def main():
     rclpy.init()
     ip = ImagePublisher('/home/albert/ros2_ws/src/task_5/resource/lab3_video.avi')
-    ip.destroy_node()
-    rclpy.shutdown()
+    
+    try:
+        ip.processImage()
+        # rclpy.spin(ip)
+    except KeyboardInterrupt:
+        pass
+    finally:
+            ip.destroy_node()
+            rclpy.shutdown()
+    
+    # rclpy.spin(ip)
+    # ip.destroy_node()
+    # rclpy.shutdown()
     
 if __name__ == '__main__':
     main()
--- src/task_5/task_5/object_detector.py
+++ src/task_5/task_5/object_detector.py
@@ -3,13 +3,16 @@ from rclpy.node import Node
 from sensor_msgs.msg import Image
 from vision_msgs.msg import BoundingBox2D
 from cv_bridge import CvBridge
-import cv2 as cv
+import cv2
+from matplotlib import pyplot as plt
+import numpy as np
+
 
 class image():
     def __init__(self):
         self.ros2 = None
         self.cv2 = None
-        self.gray = None
+        self.hsv = None        
         
 class ObjectDetector(Node):
     def __init__(self):
@@ -20,12 +23,50 @@ class ObjectDetector(Node):
         self.bridge = CvBridge()
         
     def objectDetect(self,msg):
-        if msg != None:
-            self.get_logger.info(f'Received new image : {msg}')
+        
+        # step 1: Detect red color objects using hsv mask
+        # step 2: Find contour and identify triangle
+        # step 3: Draw box
+        
+        # When target's features include color, it'd better to use hsv masking
+        # Otherwise, gray scale + edge detection would be better.
+        
+        if msg != self.image.ros2:
+        
+            self.get_logger().info(f'Received new image : {msg}')
             self.image.ros2 = msg
-            self.image.cv2 = self.bridge.cv2_to_imgmsg(msg)
-            self.image.gray = cv.cvtColor(self.image.cv2, cv.COLOR_BGR2GRAY)
-            cv.imshow(self.image.gray)
+            self.image.cv2 = self.bridge.imgmsg_to_cv2(msg, desired_encoding='8UC3')
+            
+            # cv2.imshow('Frame', self.image.cv2)
+            # cv2.waitKey(25)
+
+            # convert the image from RGB into hue
+            # https://answers.opencv.org/question/184711/select-hsv-hue-from-30-to-30-in-python/
+            # https://docs.wpilib.org/en/stable/docs/software/vision-processing/wpilibpi/image-thresholding.html
+            # https://stackoverflow.com/questions/10948589/choosing-the-correct-upper-and-lower-hsv-boundaries-for-color-detection-withcv
+            self.image.hsv = cv2.cvtColor(self.image.cv2, cv2.COLOR_BGR2HSV)
+            # create color masks (hue [0, 180], saturation [0, 255], brightness [0, 255])
+            
+            # red from 0 to 20 degree
+            lower_bound1 = np.array([0, 70, 130])
+            upper_bound1 = np.array([10, 255, 255])
+            mask1 = cv2.inRange(self.image.hsv, lower_bound1, upper_bound1)
+            # red from 160 to 180 degree
+            lower_bound2 = np.array([170, 70, 130])
+            upper_bound2 = np.array([180, 255, 255])
+            mask2 = cv2.inRange(self.image.hsv, lower_bound2, upper_bound2)
+            
+            mask = mask1 + mask2
+            masked = cv2.bitwise_and(self.image.cv2, self.image.cv2, mask = mask)
+            
+            cv2.imshow('Before mask', self.image.cv2)
+            cv2.imshow('Mask', mask)
+            cv2.imshow('After mask', masked)
+            cv2.waitKey(500)
+            
+        else:
+            cv2.destroyAllWindows()
+            
             
             
         
