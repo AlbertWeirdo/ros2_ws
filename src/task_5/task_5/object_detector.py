beaf475894fe94d5e5b3838ad9e87bf41a00aeae
AlbertWeirdo <fualbert20030507@gmail.com>
2025-04-02 02:05:41 -0400

    completed finding the right contour and drawing bounding box

--- src/task_5/task_5/object_detector.py
+++ src/task_5/task_5/object_detector.py
@@ -39,36 +39,80 @@ class ObjectDetector(Node):
             
             # cv2.imshow('Frame', self.image.cv2)
             # cv2.waitKey(25)
-
-            # convert the image from RGB into hue
-            # https://answers.opencv.org/question/184711/select-hsv-hue-from-30-to-30-in-python/
-            # https://docs.wpilib.org/en/stable/docs/software/vision-processing/wpilibpi/image-thresholding.html
-            # https://stackoverflow.com/questions/10948589/choosing-the-correct-upper-and-lower-hsv-boundaries-for-color-detection-withcv
-            self.image.hsv = cv2.cvtColor(self.image.cv2, cv2.COLOR_BGR2HSV)
-            # create color masks (hue [0, 180], saturation [0, 255], brightness [0, 255])
-            
-            # red from 0 to 20 degree
-            lower_bound1 = np.array([0, 70, 130])
-            upper_bound1 = np.array([10, 255, 255])
-            mask1 = cv2.inRange(self.image.hsv, lower_bound1, upper_bound1)
-            # red from 160 to 180 degree
-            lower_bound2 = np.array([170, 70, 130])
-            upper_bound2 = np.array([180, 255, 255])
-            mask2 = cv2.inRange(self.image.hsv, lower_bound2, upper_bound2)
-            
-            mask = mask1 + mask2
-            masked = cv2.bitwise_and(self.image.cv2, self.image.cv2, mask = mask)
-            
-            cv2.imshow('Before mask', self.image.cv2)
-            cv2.imshow('Mask', mask)
-            cv2.imshow('After mask', masked)
-            cv2.waitKey(500)
-            
+            self.hsvMask()
         else:
-            cv2.destroyAllWindows()
+            cv2.destroyAllWindows()    
+        
+    
+    def hsvMask(self):
+        # convert the image from RGB into hue
+        # https://answers.opencv.org/question/184711/select-hsv-hue-from-30-to-30-in-python/
+        # https://docs.wpilib.org/en/stable/docs/software/vision-processing/wpilibpi/image-thresholding.html
+        # https://stackoverflow.com/questions/10948589/choosing-the-correct-upper-and-lower-hsv-boundaries-for-color-detection-withcv
+        self.image.hsv = cv2.cvtColor(self.image.cv2, cv2.COLOR_BGR2HSV)
+        # create color masks (hue [0, 180], saturation [0, 255], brightness [0, 255])
+        
+        # red from 0 to 20 degree
+        lower_bound1 = np.array([0, 70, 130])
+        upper_bound1 = np.array([10, 255, 255])
+        mask1 = cv2.inRange(self.image.hsv, lower_bound1, upper_bound1)
+        # red from 160 to 180 degree
+        lower_bound2 = np.array([170, 70, 130])
+        upper_bound2 = np.array([180, 255, 255])
+        mask2 = cv2.inRange(self.image.hsv, lower_bound2, upper_bound2)
+        
+        mask = mask1 + mask2
+        masked = cv2.bitwise_and(self.image.cv2, self.image.cv2, mask = mask)
+        
+        
+        cv2.imshow('Before mask', self.image.cv2)
+        cv2.imshow('Mask', mask)
+        cv2.imshow('After mask', masked)
+        imageWithBoundingBox = self.findContour(mask)
+        if imageWithBoundingBox is not None:
+            cv2.imshow("Triangle Detection", imageWithBoundingBox)
+        cv2.waitKey(25)
+        
+    def findContour(self, mask):
+        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        
+        for contour in contours:
             
+            # filter out noise
+            area = cv2.contourArea(contour)
+            if area < 1000:
+               continue
+           
+            # check if the contour is a triangle
+            # acceptable error range will be 2 percent of the contour's perimeter  
+            error = 0.02 * cv2.arcLength(contour, True)
+            approx = cv2.approxPolyDP(contour, error, True)
             
+            if len(approx) == 3:
+                # draw a green triangle outside the object 
+                cv2.drawContours(self.image.cv2, [approx], -1, (0, 255, 0), 3)
+                # draw a bounding rectangle
+                x, y, w, h = cv2.boundingRect(approx)
+                image = cv2.rectangle(self.image.cv2, (x, y), (x + w, y + h), (0, 255, 0), 3)
+                
+                # compute triangle's centroid
+                triangle_center_x = np.mean(approx[:, 0, 0])
+                triangle_center_y = np.mean(approx[:, 0, 1])
+                print(f'triangle\'s centroid is {triangle_center_x - x, triangle_center_y - y} relative to the top left corner of the frame')
+                
+                pub_msg = BoundingBox2D()
+                # pub_msg.center = triangle_center_x
+                # pub_msg.center = triangle_center_y
+                pub_msg.size_x = float(w)
+                pub_msg.size_y = float(h)
+                
+                self.publisher.publish(pub_msg)
+                
+                # cv2.imshow("Triangle Detection", self.image.cv2)
+                # cv2.waitKey(25)
+                return image
             
+                       
         
         
 def main():
